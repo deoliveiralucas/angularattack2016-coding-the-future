a1dd925f73f0331a9c73370783879d3e817f1dff
Dani <a22.daniela@gmail.com>
2016-05-13 23:44:43 -0300

    app structure

---
+++ app/mood/mood.component.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by Dani on 13/05/2016.
+ */
---
+++ app/person/person.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by Dani on 13/05/2016.
+ */
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/person/person.component.html'
+})
+export class PersonComponent implements OnInit {
+
+    ngOnInit() {
+        
+    }
+
+}
