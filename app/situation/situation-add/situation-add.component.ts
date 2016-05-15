1f679787b845c553c154cd35424e0b93d7730ba3
Dani <a22.daniela@gmail.com>
2016-05-15 04:39:48 -0300

    situation

--- app/situation/situation-add/situation-add.component.ts
+++ app/situation/situation-add/situation-add.component.ts
@@ -1,13 +1,37 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router-deprecated';
+import {SituationService} from '../../situation/situation.service';
+import {Situation} from '../../situation/situation.model';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/situation/situation-add/situation-add.component.html'
+    templateUrl: '../app/situation/situation-add/situation-add.component.html',
+    providers: [SituationService]
 })
 export class SituationAddComponent implements OnInit {
+    public situation: Situation = new Situation();
+
+    constructor(
+        private _service: SituationService,
+        private _router: Router
+    ){}
 
     ngOnInit() {
 
     }
 
+    onSubmit() {
+        var itens: any = this._service.getStorage();
+        if (itens) {
+            itens.push(this.situation);
+        } else {
+            itens = [this.situation];
+        }
+        this._service.setStorage(itens);
+        this.onclickGoToEntries();
+    }
+
+    onclickGoToEntries() {
+        this._router.navigate(['Entries']);
+    }
 }
---
+++ app/situation/situation-mock.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by Dani on 15/05/2016.
+ */
+export let SITUATION = [
+    {
+        description: 'Música',
+        id: 1
+    },
+    {
+        description: 'Amigos',
+        id: 2
+    },
+    {
+        description: 'Filmes',
+        id: 3
+    },
+    {
+        description: 'Trabalho',
+        id: 4
+    },
+    {
+        description: 'Estudo',
+        id: 5
+    },
+    {
+        description: 'Discussão',
+        id: 6
+    },
+    {
+        description: 'Briga',
+        id: 7
+    }
+];
