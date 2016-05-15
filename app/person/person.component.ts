cb3df69c5a443f009ddb13b5f3228c64d56598f2
Dani <a22.daniela@gmail.com>
2016-05-15 00:33:32 -0300

    person

--- app/person/person.component.ts
+++ app/person/person.component.ts
@@ -1,13 +1,31 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router-deprecated';
+import {PersonService} from '../person/person.service';
+import {Person} from '../person/person.model';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/person/person.component.html'
+    templateUrl: 'app/person/person.component.html',
+    providers: [PersonService]
 })
 export class PersonComponent implements OnInit {
+    public person: Person = new Person;
+    
+    constructor(
+        private _service: PersonService,
+        private _router: Router
+    ){}
 
     ngOnInit() {
         
     }
 
+    onSubmit() {
+        this._service.setStorage(this.person);
+        this.onclickGoToMood();
+    }
+
+    onclickGoToMood() {
+        this._router.navigate(['Mood']);
+    }
 }
